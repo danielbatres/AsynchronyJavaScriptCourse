827c99860ebd5a5f2044e2ed236bdc43cccb5fae
danielbatres <danielbatresuser@gmail.com>
2022-06-30 14:56:34 -0600

    Fetch data

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,5 @@
 const { url } = require("inspector");
-const XMLHttpRequest = require("xmlhttprequest");
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const API = "https://api.escuelajs.co/api/v1";
 
 const fetchData = (urlApi, callback) => {
@@ -10,11 +10,24 @@ const fetchData = (urlApi, callback) => {
         if (xhttp.readyState === 4) {
             if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error(`Error ${urlApi}`);
+                return callback(error, null);
             }
-        } else {
-            const error = new Error(`Error ${urlApi}`);
-            return callback(error, null);
         }
     }
     xhttp.send();
 } 
+
+fetchData(`${API}/products`, (error1, data1) => {
+    if (error1) return console.log(error1);
+    fetchData(`${API}/products/${data1}`, (error2, data2) => {
+        if (error2) return console.log(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, (error3, data3) => {
+            if (error3) return console.log(error3);
+            console.log(data1[0]);
+            console.log(data2.title);
+            console.log(data3.name);
+        });
+    });
+});
