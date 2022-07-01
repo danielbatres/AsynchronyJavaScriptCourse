952b31d63591c0a269468bc54998e3fce97a812a
danielbatres <danielbatresuser@gmail.com>
2022-06-30 19:48:56 -0600

    Try and catch

---
+++ src/async/challenge.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+
+const API = "https://api.escuelajs.co/api/v1";
+
+const fetchData = async urlApi => {
+    const response = await fetch(urlApi);
+    const data = await response.json();
+
+    return data;
+};
+
+const anotherFunction = async urlApi => {
+    try {
+        const products = await fetchData(`${API}/products`);
+        const product = await fetchData(`${API}/products/${products[0].id}`);
+        const category = await fetchData(`${API}/categories/${product.category.id}`);
+
+        console.log(products);
+        console.log(product.title);
+        console.log(category.name);
+    } catch {
+        console.error(error);
+    }
+}
+
+anotherFunction(API);
